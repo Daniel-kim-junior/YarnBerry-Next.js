18ab254abf13b5a8becb78a7dbd03610d48c51a1
jio-kim <now20412041@gmail.com>
2022-07-06 23:40:42 +0900

    route test

---
+++ pages/sub/index.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
+import Link from "next/link";
+
+export default function SubIndex() {
+  return (
+    <>
+      <h1>/pages/sub/index.js</h1>
+      <a href="/">/pages</a>
+    </>
+  );
+}
