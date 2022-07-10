304e315e87afbf80d3fd1163997e9d6618f0f6da
jio-kim <now20412041@gmail.com>
2022-07-11 00:00:43 +0900

    Next tutorial

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import conn from "../../lib/db";
-import { QueryResult } from "pg";
+
 
 type Data = {
   name: string;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,16 @@
-import type { NextPage } from "next";
-import Link from "next/link";
-import Nav from "./components/Nav";
-import HeadMeta from "./components/HeadMeta";
-import styled from "styled-components";
-import { useEffect, useState } from "react";
+import { Fragment } from "react";
 
-interface Post {
-  username: string | null;
-  posttitle: string | null;
-  contents: string | null;
+const dummy = [
+  {
+    id: 1,
+    title: "첫번째 게시물",
+    contents: "hi",
+    image: 
+  },
+];
+
+function HomePage() {
+  return Fragment;
 }
 
-interface Props {
-  props: Post;
-}
-
-export async function status() {
-  const res = await fetch("http://localhost:3000/api");
-  if (res.ok) {
-    const data = await res.json();
-    return data;
-  }
-}
-
-const Home: NextPage = () => {
-  const [state, setState] = useState<Post>();
-  useEffect(() => {
-    (async () => {
-      const res = await fetch("http://localhost:3000/api");
-      if (res.ok) {
-        const data = await res.json();
-        setState(data);
-      }
-    })();
-  }, []);
-  return (
-    <>
-      <HeadMeta image="" url="" description="FrontEnd" title="개발자 김민성"></HeadMeta>
-      <NavContainer>
-        <Nav />
-      </NavContainer>
-      <div>게시물 작성자 : {state?.username}</div>
-      <div>게시물 제목 : {state?.posttitle}</div>
-      <div>게시물 내용 : {state?.contents}</div>
-    </>
-  );
-};
-
-const NavContainer = styled.nav`
-  display: flex;
-  background: teal;
-`;
-
-export default Home;
+export default HomePage;
